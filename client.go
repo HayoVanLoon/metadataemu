dc9153e3a74e6328ff8c3d1f5d78dcca14e9d1e5
Hayo van Loon <hayovanloon@gmail.com>
2020-08-14 18:50:14 +0200

    include client

---
+++ client.go
@@ -0,0 +1,38 @@
+package metadataemu
+
+import (
+	"cloud.google.com/go/compute/metadata"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Client interface {
+	Get(path string) (string, error)
+}
+
+type client struct {
+	port   string
+	apiKey string
+	real   bool
+}
+
+func NewClient(port, apiKey string, live bool) Client {
+	if live {
+		return &metadata.Client{}
+	}
+	return &client{port: port, apiKey: apiKey}
+}
+
+func (c *client) Get(path string) (string, error) {
+	if c.apiKey == "" {
+		return "", nil
+	}
+	url := fmt.Sprintf("http://localhost:%s%s&apiKey=%s", c.port, path, c.apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("could not query metadata emulator: %s", err)
+	}
+	bs, err := ioutil.ReadAll(resp.Body)
+	return string(bs), err
+}
